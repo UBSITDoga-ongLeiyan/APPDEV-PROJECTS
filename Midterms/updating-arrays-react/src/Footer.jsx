a948779a747a6bc308961e269a58f7b9764bb59c
UBSITDoga-ongLeiyan <98291712+UBSITDoga-ongLeiyan@users.noreply.github.com>
2024-11-13 13:46:24 +0800

    Delete Midterms directory

--- Midterms/updating-arrays-react/src/Footer.jsx
+++
@@ -1,8 +0,0 @@
-export default function Footer() {
-    return(
-        <footer>
-            <hr />
-            <p>&copy; {new Date() .getFullYear()} My Games Website || Written by: Leiyan M. Doga-ong </p>
-        </footer>
-    )
-}
--- Midterms/updating-arrays-react/src/Header.jsx
+++
@@ -1,7 +0,0 @@
-export default function Header() {
-    return(
-        <header>
-            <h1> This is My List of Favorite Games: </h1>
-        </header>
-    )
-}
